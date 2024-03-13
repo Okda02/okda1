9c212ccd99bbbc1fbe75794cf18c66d41804ce36
Okda02 <anna.dolmond@mail.ru>
2024-03-13 14:09:53 +0300

    first commit

---
+++ js.js
@@ -0,0 +1,42 @@
+var imd_so_icon_1 = document.getElementById('so_icon_1');
+var isImage1_1=true;
+function osp(){
+    var op_op = document.getElementById('op_op');
+    op_op.style.display = (op_op.style.display === 'none' || op_op.style.display === '') ? 'block' : 'none';
+    if(op_op.style.display == 'block'){
+        document.getElementById('so_icon_1').src='/img/icons8-сердце-30_.png';
+    }
+    else{
+        document.getElementById('so_icon_1').src='/img/icons8-сердце-30 (1).png';     
+    }
+}
+function sop(){
+    var op_op = document.getElementById('so_so');
+    op_op.style.display = (op_op.style.display === 'none' || op_op.style.display === '') ? 'block' : 'none';
+    if(op_op.style.display == 'block'){
+        document.getElementById('so_icon_2').src='/img/icons8-сердце-30_.png';
+    }
+    else{
+        document.getElementById('so_icon_2').src='/img/icons8-сердце-30 (1).png';     
+    }
+}
+function pso(){
+    var op_op = document.getElementById('pr_pr');
+    op_op.style.display = (op_op.style.display === 'none' || op_op.style.display === '') ? 'block' : 'none';
+    if(op_op.style.display == 'block'){
+        document.getElementById('so_icon_3').src='/img/icons8-сердце-30_.png';
+    }
+    else{
+        document.getElementById('so_icon_3').src='/img/icons8-сердце-30 (1).png';     
+    }
+}
+function ppp(){
+    var p_p = document.getElementById('p_p');
+    p_p.style.display = (p_p.style.display === 'none' || p_p.style.display === '') ? 'block' : 'none';
+    if(p_p.style.display == 'block'){
+        document.getElementById('so_icon_3').src='/img/icons8-сердце-30_.png';
+    }
+    else{
+        document.getElementById('so_icon_3').src='/img/icons8-сердце-30 (1).png';     
+    }
+}
